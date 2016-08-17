ad03de02f345aed62d5aa64f61fbc36d37dc944f
guillaume <guillaume.carbajal@gmail.com>
2016-08-17 15:34:32 +0200

    Rearange array function

--- myfirstprogram.cc
+++ myfirstprogram.cc
@@ -8,29 +8,31 @@ using namespace std;
 
 int main(){
   const int size = 3, order = 4;
-  const size_t size_, order_;
-  size_(size), order_(order);
+  //const size_t order_;
+  //order_(order);
 
   Real x[size * order];
   Real *new_x;
   new_x = FastMalloc(size * order);
-  /*for (size_t i = 0; i < size * order_; i++) {
+  for (size_t i = 0; i < size * order; i++) {
     x[i] = i + 1;
+    new_x[size * (i % order) + i / order] = x[i];
   }
-  FastCopy(x, sizeof(x), new_x);
-  for (int i = 0; i < sizeof(x); i++) {
+  for (size_t i = 0; i < size * order; i++) {
     cout << x[i]<<'\n';
   }
-  for (int i = 0; i < sizeof(x); i++) {
-    count << new_x[i];
-  }*/
+  for (size_t i = 0; i < size * order; i++) {
+    cout << new_x[i]<<'\n';
+  }
   cout << size << '\n'
-       << order_ << '\n'
+       << order << '\n'
        << typeid(size).name() << '\n'
        << typeid(x).name() << '\n'
        //<< typeid(x_new).name() << '\n'
        << endl;
-       
-  cout << sizeof(x) <<endl;
+  cout << sizeof(x) / sizeof(x[0]) << '\n' // nb of elements of x
+       << sizeof(Real) << '\n'             // size of a Real type
+       << sizeof(new_x)<< '\n' 
+       << sizeof(x) <<endl;                // nb of bits of x
   return 0;
 }
