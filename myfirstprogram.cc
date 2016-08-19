0e345a16be7dab15f7862d8b55ab97d51b594cab
guillaume <guillaume.carbajal@gmail.com>
2016-08-19 16:10:26 +0200

    back to new_x and new_x_t version

--- myfirstprogram.cc
+++ myfirstprogram.cc
@@ -1,5 +1,6 @@
 #include <memory>
 #include "fast.h"
+#include "random.h"
 #include <iostream>
 #include <typeinfo>
 #include <string>
@@ -7,32 +8,82 @@
 using namespace std;
 
 int main(){
-  const int size = 3, order = 4;
-  //const size_t order_;
-  //order_(order);
+  const int size = 4, order = 3, word_dimension = 10;
+  const int input_dimension = order * word_dimension;
+  const int output_dimension = 20;
+  const int max_sequence_length = 9;
 
-  Real x[size * order];
+  Real x[size * input_dimension];
+
   Real *new_x;
-  new_x = FastMalloc(size * order);
-  for (size_t i = 0; i < size * order; i++) {
-    x[i] = i + 1;
-    new_x[size * (i % order) + i / order] = x[i];
+  new_x = FastMalloc(size * input_dimension);
+
+  Real *new_x_t;
+
+  Real *current_slice;
+  current_slice = FastMalloc(word_dimension * size);
+
+  Real *b_t_;
+  b_t_ = FastMalloc(output_dimension * size * max_sequence_length);
+
+  Real *weights_;
+  weights_ = FastMalloc(output_dimension * word_dimension);
+
+  const uint32_t seed = 1;
+  Random random(seed);
+
+
+  // Fill x and new_x
+  for (size_t i = 0; i < order; i++) {
+    const int word_position = word_dimension * (size * (i % order) + i / order);
+    const int new_word_position = i * word_dimension;
+    cout << i << endl;
+    // Just fill x here using new_word_position
+    FastAddConstant(&x[new_word_position], word_dimension, i + 1, &x[new_word_position]);
+    FastCopy(&x[word_position], word_dimension, &new_x[new_word_position]);
   }
-  for (size_t i = 0; i < size * order; i++) {
-    cout << x[i]<<'\n';
+
+  new_x_t = new_x;
+
+  // Initialize random numbers
+  const Real mu = 0.;
+  const Real sigma = 0.1;
+
+
+  random.ComputeGaussianRandomNumbers(output_dimension * word_dimension,
+                                      mu,
+                                      sigma,
+                                      weights_);
+  // Display weights_ matrix
+  /*
+     for (size_t i = 0; i < output_dimension; i++) {
+     for (size_t j = 0; j < word_dimension; j++) {
+     cout << weights_[j + i * word_dimension] << ' ';
+     }
+     cout << '\n';
+     }*/
+
+  FastMatrixMatrixMultiply(1.0,
+                           weights_,
+                           false,
+                           output_dimension,
+                           word_dimension, // NB: input_dimension = word_dimension
+                           new_x_t,
+                           false,
+                           size,
+                           b_t_);
+  // Display output b_t_
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < output_dimension; j++) {
+      cout << b_t_[j + i * output_dimension] << ' ';
+    }
+    cout << '\n';
   }
-  for (size_t i = 0; i < size * order; i++) {
-    cout << new_x[i]<<'\n';
-  }
-  cout << size << '\n'
-       << order << '\n'
-       << typeid(size).name() << '\n'
-       << typeid(x).name() << '\n'
-       //<< typeid(x_new).name() << '\n'
-       << endl;
-  cout << sizeof(x) / sizeof(x[0]) << '\n' // nb of elements of x
-       << sizeof(Real) << '\n'             // size of a Real type
-       << sizeof(new_x)<< '\n' 
-       << sizeof(x) <<endl;                // nb of bits of x
+
+
+  // TO DO: finish the loop by adding the output after order computation
+
+
+
   return 0;
 }
